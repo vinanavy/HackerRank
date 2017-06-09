1c294267d99cd91a1c5a1fde0f849431002863e0
Vina Navy <vinanavy@gmail.com>
2017-06-09 22:04:44 +0700

    Day 0 - Hello World

---
+++ DayZero_HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+	static void Main(string[] args)
+	{
+		string inputString;
+		inputString = Console.ReadLine();
+		Console.WriteLine("Hello World");
+		Console.WriteLine(inputString);
+	}
+}
